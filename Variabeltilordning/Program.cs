ead30bc4115238281162037277608f52259b31ff
Lonatri <gyda-94@hotmail.com>
2025-11-18 12:30:32 +0100

    Add project files.

---
+++ Variabeltilordning/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine($"This is the sum of the two numbers: {AddTwoNumbers()}");
+
+static int AddTwoNumbers()
+{
+    int number1 = 2;
+    int number2 = 3;
+    return number1 + number2;
+}
